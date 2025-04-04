284802c83afac9ed89e87bc148efc453ad1067b2
Naga Karthik <emvnagakarthik@gmail.com>
2025-04-04 13:17:32 -0400

    add comparison with seg_ms_lesion

--- baselines/comparison_with_other_methods_lesion.sh
+++ baselines/comparison_with_other_methods_lesion.sh
@@ -72,49 +72,20 @@ echo "PATH_LOG: ${PATH_LOG}"
 echo "PATH_QC: ${PATH_QC}"
 
 SUBJECT=$1
-PATH_NNUNET_SCRIPT=$2
-PATH_NNUNET_MODEL=$3
+DATASET=$2
+PATH_STACKING_SCRIPT=$3
+PATH_NNUNET_SCRIPT="/home/GRAMES.POLYMTL.CA/u114716/tum-poly/miccai_amai_lesions_spine/packaging/run_inference_single_subject.py"
 
 echo "SUBJECT: ${SUBJECT}"
+echo "DATASET: ${DATASET}"
 echo "PATH_NNUNET_SCRIPT: ${PATH_NNUNET_SCRIPT}"
-echo "PATH_NNUNET_MODEL: ${PATH_NNUNET_MODEL}"
+# echo "PATH_NNUNET_MODEL: ${PATH_NNUNET_MODEL}"
 
 # ------------------------------------------------------------------------------
 # CONVENIENCE FUNCTIONS
 # ------------------------------------------------------------------------------
 # Get ANIMA binaries path
 anima_binaries_path=$(grep "^anima = " ~/.anima/config.txt | sed "s/.* = //" | sed 's/\/$//')
-
-# Segment lesion using our nnUNet model
-segment_lesion_nnUNet(){
-  local file="$1"
-  local kernel="$2"     # 2d or 3d
-
-  FILELESION="${file}_lesion_nnunet_${kernel}"
-
-  # get the GT sc seg to be used for QC
-  FILEGTSEG="${PATH_DATA}/derivatives/labels/${SUBJECT}/anat/${file}_seg-manual"
-
-  # Get the start time
-  start_time=$(date +%s)
-  # Run lesion segmentation
-  python ${PATH_NNUNET_SCRIPT} -i ${file}.nii.gz -o ${FILELESION}.nii.gz -path-model ${PATH_NNUNET_MODEL}/nnUNetTrainerDiceCELoss_noSmooth__nnUNetPlans__${kernel} -pred-type lesion
-  # Get the end time
-  end_time=$(date +%s)
-  # Calculate the time difference
-  execution_time=$(python3 -c "print($end_time - $start_time)")
-  echo "${FILELESION},${execution_time}" >> ${PATH_RESULTS}/execution_time.csv
-
-  if [[ $SUBJECT =~ "sub-zh" ]]; then
-    # Generate sagittal QC report
-    sct_qc -i ${file}.nii.gz -s ${FILEGTSEG}.nii.gz -d ${FILELESION}.nii.gz -p sct_deepseg_lesion -plane sagittal -qc ${PATH_QC} -qc-subject ${SUBJECT}
-  else
-    # Generate axial QC report
-    sct_qc -i ${file}.nii.gz -s ${FILEGTSEG}.nii.gz -d ${FILELESION}.nii.gz -p sct_deepseg_lesion -plane axial -qc ${PATH_QC} -qc-subject ${SUBJECT}
-  fi
-  # Compute ANIMA segmentation performance metrics
-  compute_anima_metrics ${FILELESION} ${file}_lesion-manual.nii.gz
-}
 
 # Compute ANIMA segmentation performance metrics
 compute_anima_metrics(){
@@ -134,6 +105,62 @@ compute_anima_metrics(){
   ${anima_binaries_path}/animaSegPerfAnalyzer -i ${FILELESION}_updated_header.nii.gz -r ${file}_lesion-manual.nii.gz -o ${PATH_RESULTS}/${FILELESION} -d -s -l -X
 
   rm ${FILELESION}_updated_header.nii.gz
+}
+
+
+# Segment spinal cord using methods available in SCT (sct_deepseg_sc or sct_propseg)
+segment_lesion() {
+  local file="$1"
+  local contrast="$2"
+  local model_type="$3"   # single_site or two_site
+  local kernel="2d"   # using 2D for TUM models by default
+
+  # Segment lesion
+  # FILELESION="${file}_lesion_deepseg"
+  FILELESION="${file}_lesion_tum_${model_type}"
+
+  if [[ $model_type == 'single_site' ]]; then
+    PATH_NNUNET_MODEL="/home/GRAMES.POLYMTL.CA/u114716/nnunet-v2/nnUNet_results/Dataset901_tumMSChunksRegion"
+  elif [[ $model_type == 'two_site' ]]; then
+    PATH_NNUNET_MODEL="/home/GRAMES.POLYMTL.CA/u114716/nnunet-v2/nnUNet_results/Dataset910_tumMSChunksPolyNYUAxialRegion"
+  fi
+
+  # Get the start time
+  start_time=$(date +%s)
+  # Run SC segmentation
+  # sct_deepseg_lesion -i ${file}.nii.gz -ofolder . -c ${contrast} -centerline svm
+  python ${PATH_NNUNET_SCRIPT} -i ${file}.nii.gz -o ${FILELESION}.nii.gz -path-model ${PATH_NNUNET_MODEL}/nnUNetTrainerDiceCELoss_noSmooth__nnUNetPlans__${kernel} -pred-type lesion -use-gpu
+  # Get the end time
+  end_time=$(date +%s)
+  # Calculate the time difference
+  execution_time=$(python3 -c "print($end_time - $start_time)")
+  echo "${FILELESION},${execution_time}" >> ${PATH_RESULTS}/execution_time.csv
+
+}
+
+
+# Segment lesion using our nnUNet model
+segment_ms_lesion_agnostic(){
+  local file="$1"
+  # local kernel="$2"     # 2d or 3d
+
+  FILELESION="${file}_lesion_seg_plb"
+  # FILELESION="${file}_lesion_seg_tum"
+
+  # Get the start time
+  start_time=$(date +%s)
+  # Run lesion segmentation
+  # python ${PATH_NNUNET_SCRIPT} -i ${file}.nii.gz -o ${FILELESION}.nii.gz -path-model ${PATH_NNUNET_MODEL}/nnUNetTrainerDiceCELoss_noSmooth__nnUNetPlans__${kernel} -pred-type lesion
+  SCT_USE_GPU=1 sct_deepseg -task seg_ms_lesion -i ${file}.nii.gz -o ${FILELESION}.nii.gz -qc ${PATH_QC} -qc-subject ${SUBJECT}
+  # SCT_USE_GPU=1 sct_deepseg -task seg_sc_ms_lesion_axial_t2w -i ${file}.nii.gz -o ${FILELESION}.nii.gz -qc ${PATH_QC} -qc-subject ${SUBJECT}
+  # Get the end time
+  end_time=$(date +%s)
+  # Calculate the time difference
+  execution_time=$(python3 -c "print($end_time - $start_time)")
+  echo "${FILELESION},${execution_time}" >> ${PATH_RESULTS}/execution_time.csv
+
+  # # Compute ANIMA segmentation performance metrics
+  # compute_anima_metrics ${FILELESION} ${file}_lesion-manual.nii.gz
 }
 
 # Copy GT lesion segmentation (located under derivatives/labels)
@@ -168,12 +195,7 @@ cd $PATH_DATA_PROCESSED
 # Copy source T2w images
 # Note: we use '/./' in order to include the sub-folder 'ses-0X'
 # We do a substitution '/' --> '_' in case there is a subfolder 'ses-0X/'
-if [[ $SUBJECT =~ "sub-zh" ]]; then
-  # for sci-zurich, copy only sagittal T2w to save space
-  rsync -Ravzh ${PATH_DATA}/./${SUBJECT}/anat/${SUBJECT//[\/]/_}_*sag_T2w.* .
-else
-  rsync -Ravzh ${PATH_DATA}/./${SUBJECT}/anat/${SUBJECT//[\/]/_}_*T2w.* .
-fi
+rsync -Ravzh ${PATH_DATA}/./${SUBJECT}/anat/${SUBJECT//[\/]/_}_acq-ax_*T2w.nii.gz .
 
 # Go to subject folder for source images
 cd ${SUBJECT}/anat
@@ -181,31 +203,77 @@ cd ${SUBJECT}/anat
 # ------------------------------------------------------------------------------
 # T2w
 # ------------------------------------------------------------------------------
-# sci-zurich
-if [[ $SUBJECT =~ "sub-zh" ]]; then
+
+if [[ $DATASET == "bavaria-quebec-spine-ms-unstitched" ]]; then
+
+  chunks="chunk-1 chunk-2 chunk-3"
+  # Loop across chunks
+  for chunk in ${chunks}; do
     # We do a substitution '/' --> '_' in case there is a subfolder 'ses-0X/'
-    file_t2="${SUBJECT//[\/]/_}"_acq-sag_T2w
-# sci-colorado
-else
-    file_t2="${SUBJECT}"_T2w
+    file_t2=${SUBJECT//[\/]/_}_acq-ax_${chunk}_T2w
+
+    # Copy GT lesion segmentation
+    copy_gt "${file_t2}"
+
+    # # binarize the GT with thr 0.5
+    # sct_maths -i ${file_t2}_lesion-manual.nii.gz -bin 0.5 -o ${file_t2}_lesion-manual.nii.gz
+
+    # Check if file_t2 exists
+    if [[ ! -e ${file_t2}.nii.gz ]]; then
+        echo "File ${file_t2}.nii.gz does not exist" >> ${PATH_LOG}/missing_files.log
+        echo "ERROR: File ${file_t2}.nii.gz does not exist. Exiting."
+        exit 1
+    fi
+
+    # Segment lesion using different methods and compute ANIMA segmentation performance metrics
+    # ms-lesion-agnostic modle
+    CUDA_VISIBLE_DEVICES=3 segment_ms_lesion_agnostic "${file_t2}"
+    # # TUM axial T2w MS lesion seg model
+    # CUDA_VISIBLE_DEVICES=3 segment_lesion "${file_t2}" 't2_ax' 'single_site'
+    # CUDA_VISIBLE_DEVICES=3 segment_lesion "${file_t2}" 't2_ax' 'two_site'
+    # # sct_deepseg_lesion model
+    # segment_lesion ${file_t2} 't2_ax'
+  done
+
+  # find . -type f -name "*_seg.nii.gz" -exec rm {} \;
+
+  echo "====================================================================================================="
+  echo "Stacking individual chunks of prediction and reference ..."
+  echo "====================================================================================================="
+
+  # Stack individual chunk predictions
+  python ${PATH_STACKING_SCRIPT} -prediction ${PATH_DATA_PROCESSED}/${SUBJECT}/anat -reference ${PATH_DATA_PROCESSED}/${SUBJECT}/anat
+
+  # # Compute ANIMA segmentation performance metrics
+  # compute_anima_metrics ${FILELESION} ${file}_lesion-manual.nii.gz
+
+
+elif [[ $DATASET == "sct-testing-large" ]]; then 
+
+  # We do a substitution '/' --> '_' in case there is a subfolder 'ses-0X/'
+  file_t2=${SUBJECT//[\/]/_}_acq-ax_T2w
+
+  # Copy GT lesion segmentation
+  copy_gt "${file_t2}"
+
+  # binarize the GT with thr 0.5
+  sct_maths -i ${file_t2}_lesion-manual.nii.gz -bin 0.5 -o ${file_t2}_lesion-manual.nii.gz
+
+  # Check if file_t2 exists
+  if [[ ! -e ${file_t2}.nii.gz ]]; then
+      echo "File ${file_t2}.nii.gz does not exist" >> ${PATH_LOG}/missing_files.log
+      echo "ERROR: File ${file_t2}.nii.gz does not exist. Exiting."
+      exit 1
+  fi
+
+  # Segment lesion using different methods and compute ANIMA segmentation performance metrics
+  CUDA_VISIBLE_DEVICES=2 segment_ms_lesion_agnostic "${file_t2}"
+  # # TUM axial T2w MS lesion seg model
+  # CUDA_VISIBLE_DEVICES=3 segment_lesion "${file_t2}" 't2_ax' 'single_site'
+  # CUDA_VISIBLE_DEVICES=3 segment_lesion "${file_t2}" 't2_ax' 'two_site'
+  # segment_lesion ${file_t2} 't2_ax'
+
 fi
-
-# Copy GT lesion segmentation
-copy_gt "${file_t2}"
-
-# binarize the GT with thr 0.5
-sct_maths -i ${file_t2}_lesion-manual.nii.gz -bin 0.5 -o ${file_t2}_lesion-manual.nii.gz
-
-# Check if file_t2 exists
-if [[ ! -e ${file_t2}.nii.gz ]]; then
-    echo "File ${file_t2}.nii.gz does not exist" >> ${PATH_LOG}/missing_files.log
-    echo "ERROR: File ${file_t2}.nii.gz does not exist. Exiting."
-    exit 1
-fi
-
-# Segment lesion using different methods and compute ANIMA segmentation performance metrics
-segment_lesion_nnUNet "${file_t2}" '2d'
-segment_lesion_nnUNet "${file_t2}" '3d_fullres'
 
 # ------------------------------------------------------------------------------
 # End
